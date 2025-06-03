08caced8f3ed72b27bdcc3f6eae071d2fdf2d6be
alwinsNest <alwintv5018@gmail.com>
2025-06-03 22:12:21 +0530

    Configure Vite build settings, including output directory and chunking for vendor libraries; update xGamma icon asset.

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,16 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: false,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom'],
+        }
+      }
+    }
+  }
 })
