66ecd5d119bc825dac437befec02bbe83c18b370
alwinsNest <alwintv5018@gmail.com>
2025-06-04 09:26:48 +0530

    Update Vite configuration for improved formatting and consistency; adjust manual chunking settings for vendor libraries.

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -12,8 +12,8 @@ export default defineConfig({
       output: {
         manualChunks: {
           vendor: ['react', 'react-dom'],
-        }
-      }
-    }
-  }
-})
+        },
+      },
+    },
+  },
+});
