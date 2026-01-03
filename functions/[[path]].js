fc13f79e7c5ee2a5d597e1cd33d3eb860ece88a7
alwin t varghese <alwintv5018@gmail.com>
2026-01-03 06:29:55 +0530

    feat: Add custom preview image for article metadata in xGamma project and Google Sign-in series

--- functions/[[path]].js
+++ functions/[[path]].js
@@ -9,10 +9,12 @@ export async function onRequest(context) {
     // You can fetch this from an API, or use a switch statement
     let customTitle = "xG: Free products for the AI race.";
     let customDescription = "xGamma is a project that aims to create open source applications for the AI age.";
+    let customPreviewImage = "/src/assets/xgamma-icon.png"
 
     if (url.pathname.includes("article/google-auth-the-kmp-way")) {
         customTitle = "Google Sign-in: The KMP Way";
         customDescription = "This is a series of articles exploring a singular Kotlin UI development project. The aim is to demonstrate integration of native libraries specific to iOS/Android while keepign the UI layer lightweight and cross-platform.";
+        customPreviewImage = "/src/articles/GoogleAuthTheKmpWay/kmp_client.png"
     }
 
     // 3. Rewrite the HTML on the fly
@@ -30,6 +32,11 @@ export async function onRequest(context) {
         .on('meta[property="og:title"]', {
             element(element) {
                 element.setAttribute("content", customTitle);
+            },
+        })
+        .on('link[rel="icon"]', {
+            element(element) {
+                element.setAttribute("href", customPreviewImage);
             },
         })
         .transform(response);
